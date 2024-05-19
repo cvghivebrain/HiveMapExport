05cf5fdfaf65c2a4aabccd3aa7e6f85f8456434f
cvghivebrain <66194501+cvghivebrain@users.noreply.github.com>
2024-05-19 22:08:07 +0100

    Opens ini file, reads contents

---
+++ HiveMapExport.dpr
@@ -0,0 +1,103 @@
+program PHiveMapExport;
+
+{$APPTYPE CONSOLE}
+
+uses
+  Classes,
+  SysUtils,
+  Windows,
+  Vcl.Graphics,
+  Vcl.Imaging.pngimage,
+  FileFunc in 'FileFunc.pas',
+  ExplodeFunc in 'ExplodeFunc.pas';
+
+var
+  i, spritecount, piececount: integer;
+  outfolder, inipath, s: string;
+  inifile: textfile;
+  PNG: TPNGImage;
+  palarray: array[0..63] of TColor;
+  spritenames: array of string;
+  spritetable, piecetable: array of integer;
+
+{ Convert RGB hex string to TColor. }
+
+function StrToTColor(str: string): TColor;
+var c: integer;
+begin
+  if str = '' then c := 0
+    else c := StrToInt('$'+str);
+  result := RGB(c shr 16, (c shr 8) and $FF, c and $FF);
+end;
+
+begin
+
+  { Program start }
+
+  if ParamCount < 1 then // Check if program was run with parameters.
+    begin
+    WriteLn('Usage: HiveMapExport inifile outfolder');
+    exit;
+    end;
+  if not FileExists(ParamStr(1)) then // Check if ini file exists.
+    begin
+    WriteLn('File not found.');
+    exit;
+    end;
+  inipath := ExtractFilePath(ParamStr(1));
+  outfolder := ParamStr(2);
+  if outfolder = '' then outfolder := inipath // Use folder of ini file.
+  else
+    begin
+    if not DirectoryExists(outfolder) then CreateDir(outfolder); // Create output folder if needed.
+    outfolder := outfolder+'\'; // Append backslash.
+    end;
+
+  { Open ini and extract data. }
+
+  spritecount := 0;
+  piececount := 0;
+  AssignFile(inifile,ParamStr(1)); // Open ini file.
+  Reset(inifile);
+  while not eof(inifile) do
+    begin
+    ReadLn(inifile,s);
+    if AnsiPos('image=',s) = 1 then
+      begin
+      PNG := TPNGImage.Create; // Initialise PNG.
+      PNG.LoadFromFile(inipath+Explode(s,'image=',1)); // Load PNG.
+      end
+    else if AnsiPos('palette=',s) = 1 then
+      begin
+      s := Explode(s,'palette=',1);
+      for i := 0 to 63 do
+        begin
+        if Explode(s,',',i) = '' then break; // Stop at end of palette.
+        palarray[i] := StrToTColor(Explode(s,',',i)); // Write palette.
+        end;
+      end
+    else if AnsiPos('sprite=',s) = 1 then
+      begin
+      s := Explode(s,'sprite=',1);
+      SetLength(spritenames,Length(spritenames)+1); // Add sprite.
+      SetLength(spritetable,Length(spritetable)+4);
+      spritenames[spritecount] := Explode(s,',',0); // Sprite name.
+      spritetable[spritecount*4] := StrToInt(Explode(s,',',1)); // Sprite x pos.
+      spritetable[(spritecount*4)+1] := StrToInt(Explode(s,',',2)); // Sprite y pos.
+      spritetable[(spritecount*4)+2] := StrToInt(Explode(s,',',3)); // Sprite width.
+      spritetable[(spritecount*4)+3] := StrToInt(Explode(s,',',4)); // Sprite height.
+      Inc(spritecount);
+      end
+    else if AnsiPos('piece=',s) = 1 then
+      begin
+      s := Explode(s,'piece=',1);
+      SetLength(piecetable,Length(piecetable)+4); // Add piece.
+      piecetable[piececount*4] := StrToInt(Explode(s,',',0)); // Piece x pos.
+      piecetable[(piececount*4)+1] := StrToInt(Explode(s,',',1)); // Piece y pos.
+      piecetable[(piececount*4)+2] := StrToInt(Explode(s,',',2)); // Piece palette.
+      piecetable[(piececount*4)+3] := StrToInt(Explode(s,',',3)); // Piece size.
+      Inc(piececount);
+      end;
+    end;
+  CloseFile(inifile);
+end.
