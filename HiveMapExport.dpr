92aa7162de24ca93b60f0a5aa4e834ac0ad70151
cvghivebrain <66194501+cvghivebrain@users.noreply.github.com>
2024-05-25 00:44:10 +0100

    Export sprite graphics data

--- HiveMapExport.dpr
+++ HiveMapExport.dpr
@@ -12,13 +12,17 @@ uses
   ExplodeFunc in 'ExplodeFunc.pas';
 
 var
-  i, spritecount, piececount: integer;
+  i, j, spritecount, piececount: integer;
   outfolder, inipath, s: string;
   inifile: textfile;
   PNG: TPNGImage;
   palarray: array[0..63] of TColor;
   spritenames: array of string;
   spritetable, piecetable: array of integer;
+
+const
+  piecewidth: array[0..15] of integer = (8,8,8,8,16,16,16,16,24,24,24,24,32,32,32,32);
+  pieceheight: array[0..15] of integer = (8,16,24,32,8,16,24,32,8,16,24,32,8,16,24,32);
 
 { Convert RGB hex string to TColor. }
 
@@ -28,6 +32,57 @@ begin
   if str = '' then c := 0
     else c := StrToInt('$'+str);
   result := RGB(c shr 16, (c shr 8) and $FF, c and $FF);
+end;
+
+{ Find out if piece is inside sprite. }
+
+function PieceInSprite(p, s: integer): boolean;
+var x, y, w, h: integer;
+begin
+  result := false; // Assume piece isn't in sprite.
+  x := spritetable[s*4]-spritetable[(s*4)+2]; // Get position of sprite.
+  y := spritetable[(s*4)+1]-spritetable[(s*4)+3];
+  w := spritetable[(s*4)+2]*2; // Get width/height of sprite.
+  h := spritetable[(s*4)+3]*2;
+  if piecetable[p*4] < x then exit; // Piece is left of sprite.
+  if piecetable[(p*4)+1] < y then exit; // Piece is above sprite.
+  if piecetable[p*4]+piecewidth[piecetable[(p*4)+3]] >= x+w then exit; // Piece is right of sprite.
+  if piecetable[(p*4)+1]+pieceheight[piecetable[(p*4)+3]] >= y+h then exit; // Piece is below sprite.
+  result := true; // Piece is inside sprite.
+end;
+
+{ Find pixel colour value in palette. }
+
+function GetPalIndex(c: TColor; p: integer): byte;
+var i: integer;
+begin
+  result := 0; // Assume transparent.
+  for i := 0 to 15 do
+    if c = palarray[(p*16)+i] then
+      begin
+      result := i; // Use matching colour instead.
+      exit;
+      end;
+end;
+
+{ Write a piece to the output file. }
+
+procedure WritePiece(x, y, p, s: integer);
+var i, j, k: integer;
+  pix: TColor;
+  pixrow: longword;
+begin
+  for i := 0 to (piecewidth[s] div 8)-1 do
+    for j := 0 to pieceheight[s]-1 do
+      begin
+      pixrow := 0;
+      for k := 0 to 7 do
+        begin
+        pix := PNG.Pixels[x+(i*8)+k,y+j]; // Read pixel from PNG.
+        pixrow := (pixrow shl 4)+GetPalIndex(pix,p); // Create longword containing 8 pixels.
+        end;
+      WriteDword(fs,pixrow); // Append longword to file.
+      end;
 end;
 
 begin
@@ -111,5 +166,18 @@ begin
     begin
     WriteLn('PNG not defined.');
     exit;
+    end;
+
+  { Read each sprite. }
+
+  for i := 0 to spritecount-1 do
+    begin
+    NewFile(0); // Start with blank file.
+    for j := 0 to piececount-1 do
+      if PieceInSprite(j,i) then // Check if piece is inside sprite.
+        begin
+        WritePiece(piecetable[j*4],piecetable[(j*4)+1],piecetable[(j*4)+2],piecetable[(j*4)+3]); // Write piece to file.
+        end;
+    if fs > 0 then SaveFile(outfolder+spritenames[i]+'.bin'); // Save sprite file if it contained pieces.
     end;
 end.
